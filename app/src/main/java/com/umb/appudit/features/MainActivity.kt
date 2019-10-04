fbedbac93e16624603840eb00b20b5c5f44b4139
cristhian rodriguez <crisdrodriguezm@hotmail.com>
2019-10-03 23:40:45 -0500

    primer commit, solo se tienen las dependencias basicas para conectarce a firebase

---
+++ app/src/main/java/com/umb/appudit/features/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.umb.appudit.features
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.umb.appudit.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
