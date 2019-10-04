0bb444506dde1aff8553dace4d2314ff8077ebef
crirodriguez <crirodriguez@wi-mobile.com>
2019-10-04 08:27:43 -0500

    primer diseño del selector de la norma

--- app/src/main/java/com/umb/appudit/features/norm/NormActivity.kt
+++ app/src/main/java/com/umb/appudit/features/norm/NormActivity.kt
@@ -1,14 +1,14 @@
-package com.umb.appudit.features
+package com.umb.appudit.features.norm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.umb.appudit.R
 
-class MainActivity : AppCompatActivity() {
+class NormActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_norm)
 
     }
 }
